23701b1fcbde47666e01d5df0c96e13b3c847819
Your Name <you@example.com>
2023-04-10 20:58:27 +0200

    refactor

--- sw.js
+++ sw.js
@@ -1,57 +1,14 @@
-const CACHE_NAME = 'mpwav1'
-const urlsToCache = ['/']
-
-self.addEventListener('install', async (event) => {
-  console.log('installing!')
+self.addEventListener('install', (event) => {
+  console.info('installing!')
   self.skipWaiting()
   event.waitUntil(cache_assets())
 })
 
-async function cache_assets() {
-  const cache = await self.caches.open(CACHE_NAME)
-  return cache.addAll(urlsToCache)
-}
-
-self.addEventListener('activate', async (event) => {
-  console.log('activating!')
-  event.waitUntil(delete_old_caches())
+self.addEventListener('push', (event) => {
+  let notification = event.data.json()
+  try {
+    self.registration.showNotification(notification.title, notification.options)
+  } catch (e) {
+    console.error('Push registration: ', e)
+  }
 })
-
-async function delete_old_caches() {
-  const keys = await caches.keys()
-  const deletePromises = keys
-    .filter((key) => key !== CACHE_NAME)
-    .map((key) => self.caches.delete(key))
-  return Promise.all(deletePromises)
-}
-
-self.addEventListener('fetch', (event) => {
-  console.log('fetching!')
-  event.respondWith(get_response(event.request))
-})
-
-async function get_response(request) {
-  const cache = await self.caches.open(CACHE_NAME)
-  const cached_response = await cache.match(request)
-  const pending_response = fetch(request).then((response) => {
-    cache.put(request, response.clone())
-    return response
-  })
-  return cached_response || pending_response
-}
-self.addEventListener('push', async function (event) {
-    const data = event.data.json();
-    console.log(data);
-    const title = data
-    const options = data
-    try {
-        registration.showNotification("Vibration Sample", {
-            body: "Buzz! Buzz!",
-            vibrate: [200, 100, 200, 100, 200, 100, 200],
-            tag: "vibration-sample",
-          });
-        } catch (e) {
-            console.log('errr',e )
-    }
-
-  });
